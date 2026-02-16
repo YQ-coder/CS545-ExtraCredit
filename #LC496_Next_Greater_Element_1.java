e3d32b9500c593241e9ad2d9866db19054a080ec
YQ <yaqiuyz@gmail.com>
2026-02-15 18:17:16 -0800

    Refactor nextGreaterElement method for efficiency

--- #LC496_Next_Greater_Element_1.java
+++ #LC496_Next_Greater_Element_1.java
@@ -1,21 +1,37 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        for(int i = 0; i < nums1.length; i++) {
-            int max = -1;
-            for(int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    for (int k = j + 1; k < nums2.length; k++) {
-                        if (nums2[k] > nums2[j]){
-                            max = nums2[k];
-                            break;
-                        }
-                    }
-                    break;
-                }
+        Deque<Integer> stack = new ArrayDeque<>();
+        Map< Integer, Integer> map = new HashMap<>();
+        for (int num: nums2 ){
+            while (!stack.isEmpty() && stack.peek()< num) {
+                map.put(stack.pop(), num);
             }
-            ans[i] = max;
-        }
-        return ans;
+        stack.push(num);
+    }
+    int[] ans = new int[nums1.length];
+    for (int i = 0; i < nums1.length; i++) {
+        ans[i] = map.getOrDefault(nums1[i], -1);
+    }
+    return ans;
     }
 }
+        
+    //     int[] ans = new int[nums1.length];
+    //     for(int i = 0; i < nums1.length; i++) {
+    //         int max = -1;
+    //         for(int j = 0; j < nums2.length; j++) {
+    //             if (nums1[i] == nums2[j]) {
+    //                 for (int k = j + 1; k < nums2.length; k++) {
+    //                     if (nums2[k] > nums2[j]){
+    //                         max = nums2[k];
+    //                         break;
+    //                     }
+    //                 }
+    //                 break;
+    //             }
+    //         }
+    //         ans[i] = max;
+    //     }
+    //     return ans;
+    // }
+
