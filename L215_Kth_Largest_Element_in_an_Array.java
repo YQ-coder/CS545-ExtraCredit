278f02972e1cdc2c4ee3d8d451ef86da07f81b69
YQ <yaqiuyz@gmail.com>
2026-03-08 03:42:45 -0700

    Implement Kth largest element finder using min-heap

---
+++ L215_Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num: nums) {
+            minHeap.add(num);
+        }
+        while (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
