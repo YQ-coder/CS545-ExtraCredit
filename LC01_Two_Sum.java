98e91ccb8db9f281307fde7d79ec2e7e88382992
YQ <yaqiuyz@gmail.com>
2026-03-29 03:00:59 -0700

    Implement twoSum method in Solution class

---
+++ LC01_Two_Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if (nums == null || nums.length == 0){
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++) {
+            int num2 = target - nums[i];
+            if (map.containsKey(num2)) {
+                res[0] = map.get(num2);
+                res[1] = i;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
