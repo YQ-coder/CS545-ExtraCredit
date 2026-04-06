7647b9e893210f92f87bfedf9aa2e31b96a8ebe8
YQ <yaqiuyz@gmail.com>
2026-04-06 01:52:15 -0700

    Implement containsDuplicate method in Solution class

---
+++ LC217_Contains Duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            if(count.containsKey(num)) {
+                return true;
+            } else {
+                count.put(num, 1);
+            }
+        }
+        return false;
+    }
+}
